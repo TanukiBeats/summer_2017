7d64290439f5d14aab16edb0f092b4f996823410
PRGuser <prguser@D-172-25-214-169.dhcp4.washington.edu>
2017-07-24 18:55:09 -0700

    adding notes

--- testing/online_examples/mike_bostock/scatter.js
+++ testing/online_examples/mike_bostock/scatter.js
@@ -1,26 +1,34 @@
+//Setting some variables to be used later on 
 var margin = {top: 50, right: 300, bottom: 50, left: 50 },
 	outerWidth = 1050,
 	outerHeight = 500,
 	width = outerWidth - margin.left - margin.right,
 	height = outerHeight - margin.top - margin.bottom;
 	
+//Making the scale for the x-axis
 var x = d3.scale.linear()
 	.range([0, width])
 	.nice();
 
+//Making the scale for the y-axis
 var y = d3.scale.linear()
 	.range([height, 0])
 	.nice();
 	
-var xCat = "Calories",
-	yCat = "Potassium",
-	rCat = "Protein (g)",
+//Defining more variables
+var xCat = "Calories", //x-category is calories
+	yCat = "Potassium", //y-category is potassium
+	rCat = "Protein (g)", //radius of datapoint is defined by protein content
 	colorCat = "Manufacturer";
 	
+//This first command is to convert the .csv file into an array of objects
+//ex. {city:" "seattle", state: "WA", population: "652405", land area: "83.9"}	
 d3.csv("cereal.csv", function(data) {
 	data.forEach(function(d) {
-		d.Calories = +d.Calories;
-		d.Carbs = +d.Carbs;
+//In this section, we are doing type conversions. In the array, numbers became strings which is something which we do *NOT* want
+//The property names for numbers are converted from strings to numbers. The reason some lines have square brackets is becuase the property names have spaces, the ones which don't have spaces don't need square brackets.  
+		d.Calories = +d.Calories; 
+		d.Carbs = +d.Carbs; 
 		d["Cups per Serving"] = +d["Cups per Serving"];
 		d["Dietary Fiber"] = +d["Dietary Fiber"];
 		d["Display Shelf"] = +d["Display Shelf"];
@@ -33,28 +41,34 @@ d3.csv("cereal.csv", function(data) {
 		d["Vitamens and Minerals"] = +d["Vitamens and Minerals"];
 	});
 	
+//Setting the max and mins of the axes and datapoints
 var xMax = d3.max(data, function(d) { return d[xCat]; }) * 1.05,
-	yMin = d3.min(data, function(d) { return d[xCat]; }), 
+	xMin = d3.min(data, function(d) { return d[xCat]; }), 
 	xMin = xMin > 0 ? 0 : xMin,
 	yMax = d3.max(data, function(d) { return d[yCat]; }) * 1.05,
 	yMin = d3.min(data, function(d) { return d[yCat]; }),
 	yMin = yMin > 0 ? 0 : yMin;
 	
+//Setting the domain of the x and y axes
 x.domain([xMin, xMax]);
 y.domain([yMin, yMax]);
 
+//Creating the x-axis
 var xAxis = d3.svg.axis()
 	.scale(x)
 	.orient("bottom")
 	.tickSize(-height);
 	
+//Creating the y-axis
 var yAxis = d3.svg.axis()
 	.scale(y)
 	.orient("left")
 	.tickSize(-width);
 	
+//Creating a new ordinal scale with a range of 10 ordinal categorical colors
 var color = d3.scale.category10();
 
+//Making the tooltips
 var tip = d3.tip()
 	.attr("class", "d3-tip")
 	.offset([-10, 0])
@@ -62,12 +76,15 @@ var tip = d3.tip()
 		return xCat + ": " + d[xCat] + "<br>" + yCat + ": " + d[yCat];
 	});
 	
+//Zooming in stuff
 var zoomBeh = d3.behavior.zoom()
 	.x(x)
 	.y(y)
 	.scaleExtent([0, 500])
 	.on("zoom", zoom);
 
+//The #scatter refers to an html element, specifically the div which was created in the .html
+//The div is called and an svg is added to it. Then it is moved over and the zooming function it applied
 var svg = d3.select("#scatter")
 	.append("svg")
 		.attr("width", outerWidth)
@@ -76,13 +93,16 @@ var svg = d3.select("#scatter")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 		.call(zoomBeh);
 
+//The tooltip was defined earlier on, so they are calling it here.
 svg.call(tip);
 
+//They are creating a rectangle! For something!
 svg.append("rect")
 	.attr("width", width)
 	.attr("height" height);
 
-svg.append('g")
+//The axes are having their traits defined here
+svg.append("g")
 	.classed("x axis" ,true)
 	.attr("transform", "translate(0," + height + ")")
 	.call(xAxis)
@@ -104,18 +124,21 @@ svg.append("g")
 	.style("text-anchor", "end")
 	.text(yCat);
 	
+//The svg which contains *just* the graph (and not the legend) is being made
+//It is defined as a variable for convenience
 var objects = svg.append("svg")
 	.classed("objects", true)
 	.attr("width", width)
 	.attr("height", height);
 	
+//The axes lines are being created here
 object.append("svg:line")
 	.classed("axisLine hAxisLine", true)
 	.attr("x1", 0)
 	.attr("y1", 0)
 	.attr("x2", 0)
 	.attr("y2", 0)
-	.attr("transform", "translat(0," + height + ")");
+	.attr("transform", "translate(0," + height + ")");
 	
 objects.append("svg:line")
 	.classed("axisLine vAxisLine", true)
@@ -124,6 +147,7 @@ objects.append("svg:line")
 	.attr("x2", 0)
 	.attr("y2", height);
 	
+//Creating the dots for the datapoints and their attributes, and making sure that the tooltips show up
 objects.selectAll("dot")
 	.data(data)
 	.enter().append("circle)
@@ -134,6 +158,7 @@ objects.selectAll("dot")
 	.on("mouseover", tip.show)
 	.on("mouseout", tip.hide)
 	
+//Creating the legend
 var legend = svg.select(".legend")
 	.data(color.domain())
 	.enter()
@@ -141,18 +166,22 @@ var legend = svg.select(".legend")
 	.classed("legend", true)
 	.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 	
+//Making the little colored circles which are on the legend
 legend.append("circle")
 	.attr("r", 3.5)
 	.attr("cx", width + 20)
 	.attr("fill", color);
 	
+//Putting the text with the names of the companies onto the legend
 legend.append("text")
 	.attr("x", width + 26)
 	.attr("dy", ".35em")
 	.text(function(d), { return d; });
 
+//Click on the little button to change the x-axis to "carbs" instead of "calories"
 d3.select("input").on("click", change);
 
+//When the "xAxis" button is clicked and the axis is changed, the graph is recalculated
 function change() {
 	xCat = "Carbs"
 	xMax = d3.max(data, function(d) { return d[xCat]; });
@@ -161,12 +190,14 @@ function change() {
 	zoomBeh.x(x.domain([xMin, xMax])).y(y.domain([yMin, yMax]));
 	
 	var svg = d3.select("#scatter").transition;
-	
+
+//Doing some animation stuff
 	svg.select(.x.axis).duration(750).call(xAxis).select(".label").text(xCat);
 	
 	objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
 }
 
+//I need to learn more about how the zooming function works
 function zoom() {
 	svg.select(".x.axis").call(xAxis);
 	svg.select(".y.axis").call(yAxis);
